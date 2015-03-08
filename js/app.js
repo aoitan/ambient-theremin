27fb6f3f6f3761f6e01885129e9d4df669cfa638
aoitan <aoi@nagatsuki-do.net>
2015-03-08 23:46:10 +0900

    update

--- js/app.js
+++ js/app.js
@@ -21,19 +21,23 @@ window.addEventListener('DOMContentLoaded', function() {
     // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
     // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
 
+    canvasDraw(INITVOL, INITFREQ);
+
     var calibrated = calibration(10); // 10秒キャリブレーション(起動時キャリブレーション画面を作るまでの暫定)
     calibrated.then(() => {
       initAudio();
       window.addEventListener('devicelight', changeBrightness);
       window.addEventListener('userproximity', approximation);
       document.addEventListener('touchmove', touchMove);
+
+      updateCanvas();
     });
   }
 
   var audio = {
     context: null,
     oscillator: null,
-    gain: null
+    gainNode: null
   }
 
   var INITFREQ = 3000;
@@ -54,39 +58,37 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // オシレータとゲインを生成
     audio.oscillator = audio.context.createOscillator();
-    audio.gain = audio.context.createGain();
+    audio.gainNode = audio.context.createGain();
 
     // オシレータとゲインをスピーカにつなぐ
-    audio.oscillator.connect(audio.gain);
-    audio.gain.connect(audio.context.destination);
+    audio.oscillator.connect(audio.gainNode);
+    audio.gainNode.connect(audio.context.destination);
 
     // オシレータオプションを設定する
-    audio.oscillator.type = 'square';
-    audio.oscillator.frequency.value = INITFREQ; // value in hertz
-    audio.oscillator.detune.value = 100; // value in cents
+    audio.oscillator.type = 'square'; // 矩形波
+    audio.oscillator.frequency.value = INITFREQ; // 発振周波数(Hz単位)
+    audio.oscillator.detune.value = 100; // デチューン設定(セント単位)
     audio.oscillator.start();
 
     audio.oscillator.onended = function() {
-      console.log('Your tone has now stopped playing!');
+      console.log('再生停止');
     }
 
     // ゲインを設定する
-    audio.gain.gain.value = INITVOL;
+    audio.gainNode.gain.value = INITVOL;
   }
 
   // マウス位置
-  var CurX;
-  var CurY;
+  var curX;
+  var curY;
 
   // タッチ位置が動いたらタッチ座標からゲインを設定する
   function touchMove(e) {
-    CurX = e.targetTouches.item(0).screenX;
-    CurY = e.targetTouches.item(0).screenY;
+    curX = e.targetTouches.item(0).screenX;
+    curY = e.targetTouches.item(0).screenY;
 
-    audio.gain.gain.value = (CurY/HEIGHT) * MAXVOL;
-    //console.log('cur: ' + CurY + ', h: ' + HEIGHT);
-
-    //canvasDraw();
+    audio.gainNode.gain.value = (curY/HEIGHT) * MAXVOL;
+    //console.log('cur: ' + curY + ', h: ' + HEIGHT);
   }
 
 
@@ -118,10 +120,10 @@ window.addEventListener('DOMContentLoaded', function() {
     // 最大値は超えない
     if (currentBrightness > maxBright) currentBrightness = maxBright;
 
-    // 直近10回の移動平均を取って周波数を作る
+    // 直近n回の移動平均を取って周波数を作る
     var ave = average(push(smaBrightness, currentBrightness, NUM_SMASAMPLE));
     audio.oscillator.frequency.value = (ave / maxBright) * MAXFREQ;
-    console.log('light: ' + ave + ', maxBright: ' + maxBright);
+    //console.log('light: ' + ave + ', maxBright: ' + maxBright);
   }
 
 
@@ -129,37 +131,57 @@ window.addEventListener('DOMContentLoaded', function() {
   function approximation(e) {
     if (e.near) {
       // ミュート
-      audio.gain.disconnect(audio.context.destination);
+      audio.gainNode.disconnect(audio.context.destination);
     } else {
       // ミュート解除
-      audio.gain.connect(audio.context.destination);
+      audio.gainNode.connect(audio.context.destination);
     }
   }
 
   // 画面に視覚効果を付ける
-  function random(number1,number2) {
-    var randomNo = number1 + (Math.floor(Math.random() * (number2 - number1)) + 1);
-    return randomNo;
-  }
-
   var canvas = document.querySelector('.canvas');
   canvas.width = WIDTH;
-  canvas.height = HEIGHT; 
+  canvas.height = HEIGHT;
 
   var canvasCtx = canvas.getContext('2d');
 
-  function canvasDraw() {
-    rC = Math.floor((audio.gain.gain.value/MAXVOL)*30);
+  var curcleSize = WIDTH / 8;
 
-    canvasCtx.globalAlpha = 0.2;
+  function fillCircle(x, y, r, g, b) {
+    var rC = WIDTH / 8;
 
-    for(i=1;i<=15;i=i+2) {
-      canvasCtx.beginPath();
-      canvasCtx.fillStyle = 'rgb(' + 100+(i*10) + ',' + Math.floor((audio.gain.gain.value/MAXVOL)*255) + ',' + Math.floor((audio.oscillator.frequency.value/MAXFREQ)*255) + ')';
-      canvasCtx.arc(CurX+random(0,50),CurY+random(0,50),rC/2+i,(Math.PI/180)*0,(Math.PI/180)*360,false);
-      canvasCtx.fill();
-      canvasCtx.closePath();
+    canvasCtx.beginPath();
+    canvasCtx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+    canvasCtx.arc(x, y, rC / 2, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+    canvasCtx.fill();
+    canvasCtx.closePath();
+    //console.log('r: ' + r + ', g: ' + g + ', b: ' + b);
+  }
+
+  function canvasDraw(gain, frequency) {
+    console.log('draw start gain: ' + gain + ' , freq: ' + frequency);
+    var rC = WIDTH / 8;
+    var xloop = 8;
+    var yloop = 14;
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.globalAlpha = 0.5;
+
+    for (var i = 1; i < xloop; ++i) {
+      for (var j = 1; j < yloop; ++j) {
+        var r = 60 + (j * 10);
+        var g = Math.floor((frequency / MAXFREQ) * (i / xloop) * 255);
+        var b = Math.floor((gain / MAXVOL) * (j / yloop) * 192);
+        fillCircle(i * rC, j * rC, r, g, b);
+      }
     }
+    console.log('draw end');
+  }
+
+  function updateCanvas() {
+    canvasDraw(audio.gainNode.gain.value, audio.oscillator.frequency.value);
+    setTimeout(updateCanvas, 66);
   }
 
 
